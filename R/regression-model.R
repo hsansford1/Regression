c0d6b6b320b0eb0c19787df8be07c58c14016881
hsansford1 <hs17630@bristol.ac.uk>
2021-10-07 10:21:12 +0100

    update documentation

--- R/regression-model.R
+++ R/regression-model.R
@@ -1,4 +1,14 @@
 
+#' Fits a least squares regression model
+#'
+#' @param X Design matrix of input values
+#' @param y Output values
+#'
+#' @return Least squares linear regression estimator
+#' @export
+#'
+#' @examples
+#'
 regression.fit <- function(X, y){
   return(solve(t(X)%*%X)%*%t(X)%*%y)
 }
