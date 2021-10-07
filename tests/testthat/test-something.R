bca675375ff3f7b598ac6721f36ff53879ffa081
hsansford1 <hs17630@bristol.ac.uk>
2021-10-07 12:22:32 +0100

    Update test

--- tests/testthat/test-something.R
+++ tests/testthat/test-something.R
@@ -1,6 +1,6 @@
 test_that("correct dimension", {
-  X <- matrix(data = c(2.7,50,6,3.3,58,7), nrow=3)
+  X <- matrix(data = c(2,4,6,7,1,3,5,6,5,6), ncol=2)
   X <- cbind(1, X)
-  y <- c(0.4,0.16,0.3)
-  expect_equal(length(y), length(regression.fit(X,y)))
+  y <- c(0.4,0.16,0.3,0.2,0.22)
+  expect_equal(dim(X)[2], length(regression.fit(X,y)))
 })
