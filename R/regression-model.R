55933c3a15cbbb6ab9e72ec2242f965fb96de562
hsansford1 <hs17630@bristol.ac.uk>
2021-10-07 10:39:57 +0100

    add prediction function

--- R/regression-model.R
+++ R/regression-model.R
@@ -10,6 +10,20 @@
 #' @examples
 #'
 regression.fit <- function(X, y){
-  return(solve(t(X)%*%X)%*%t(X)%*%y)
+  params <- solve(t(X)%*%X)%*%t(X)%*%y
+  return(params)
 }
 
+#' Predict output from input data
+#'
+#' @param params Least squares estimator
+#' @param data new input data
+#'
+#' @return Prediction of output values
+#' @export
+#'
+#' @examples
+regression.predict <- function(params, data){
+  data <- cbind(1, data)
+  return(data %*% params)
+}
